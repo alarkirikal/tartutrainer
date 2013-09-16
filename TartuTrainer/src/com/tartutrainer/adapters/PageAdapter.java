797fca07bad3ba9e559b21585a96a1515059b806
Alar Kirikal <alark@ut.ee>
2013-09-16 14:42:13 +0300

    Refactor commit (Android Source)
    Changed the base architecture of the program by creating new packages
    and refactoring the classes.

---
+++ TartuTrainer/src/com/tartutrainer/adapters/PageAdapter.java
@@ -0,0 +1,29 @@
+package com.tartutrainer.adapters;
+
+import java.util.List;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class PageAdapter extends FragmentPagerAdapter {
+	
+	private List<Fragment> fragments;
+	
+	public PageAdapter(FragmentManager fm, List<Fragment> fragments) {
+		super(fm);
+		this.fragments = fragments;
+	}
+
+	@Override
+	public Fragment getItem(int arg0) {
+		return this.fragments.get(arg0);
+	}
+
+	@Override
+	public int getCount() {
+		return this.fragments.size();
+	}
+	
+
+}
