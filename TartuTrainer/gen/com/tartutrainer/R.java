c8eb37da4bd4c3015fc9e4e00e9acd4625bd97a0
Alar Kirikal <alar.kirikal@gmail.com>
2013-10-02 19:59:57 +0300

    sortby commit

--- TartuTrainer/gen/com/tartutrainer/R.java
+++ TartuTrainer/gen/com/tartutrainer/R.java
@@ -29,15 +29,18 @@ public final class R {
     }
     public static final class id {
         public static final int ExerciseIcon=0x7f090003;
-        public static final int action_add=0x7f09000a;
-        public static final int action_settings=0x7f09000b;
+        public static final int action_add=0x7f09000d;
+        public static final int action_settings=0x7f09000e;
         public static final int exerciseDesc=0x7f090005;
         public static final int exerciseName=0x7f090004;
         public static final int layout2=0x7f090002;
         public static final int listAllExercises=0x7f090006;
-        public static final int listAllPrograms=0x7f090007;
-        public static final int programDesc=0x7f090009;
-        public static final int programName=0x7f090008;
+        public static final int listAllPrograms=0x7f09000a;
+        public static final int programDesc=0x7f09000c;
+        public static final int programName=0x7f09000b;
+        public static final int sortAllProgramsBy=0x7f090009;
+        public static final int sortAllProgramsByText=0x7f090008;
+        public static final int sortAllProgramsWrapper=0x7f090007;
         public static final int viewpager_main=0x7f090001;
         public static final int viewpager_second=0x7f090000;
     }
@@ -56,6 +59,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int sort_by=0x7f050002;
     }
     public static final class style {
         /** 
